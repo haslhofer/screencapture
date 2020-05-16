8fb1160ab2844da0ce5bfae2769531a54ec508dd
Dr. Gerald Haslhofer <haslhofer@outlook.com>
2020-05-16 14:48:17 -0700

    create base project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace screencapture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
